5faac23e2cb4caf72e6e4215616273f497a3c3d9
Guillermo Santos <santosguillermo360@outlook.com>
2023-08-27 18:48:31 -0400

    Create Console.Cosmos.cs
    The Cosmos Console Stream (inspired on the UnixConsoleStream class from dotnet).

---
+++ source/Cosmos.System2/Console.Cosmos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using Cosmos.Core;
+using Cosmos.System.IO;
+
+namespace Cosmos.System
+{
+    public partial class Console
+    {
+        internal class CosmosConsoleStream : ConsoleStream
+        {
+            internal CosmosConsoleStream(FileAccess access) : base(access)
+            {
+            }
+
+            public override void Flush() {}
+            public override void Write(ReadOnlySpan<byte> buffer)
+            {
+                Global.Console.Write(buffer.ToArray());
+            }
+
+            public override int Read(Span<byte> buffer) => Global.Console.StdInReader.ReadLine(buffer);
+        }
+
+        private int Read(Span<byte> buffer) => throw new NotImplementedException();
+    }
+}
+
